7e9ad6729a85eb7ed1e493e13caad3b5e8663f8b
Patricia <patriciamvbog@gmail.com>
2023-02-03 10:18:41 +0000

    added includes

--- js/main.js
+++ js/main.js
@@ -5,20 +5,43 @@ form.classList.remove('collapsed');*/
 
 /* llamamaos al html, hay que poner siempre primero*/
 const list= document.querySelector('.js-list'); 
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const descrSearchText = input_search_desc.value;
+
+
+
+const kittenOneImage = 'https://dev.adalab.es/gato-siames.webp';
+const kittenOneName = 'Anastacio';
+const kittenOneDesc =
+  ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const kittenOneRace = 'Siamés';
+
+
+const kittenTwoImage = 'https://dev.adalab.es/sphynx-gato.webp';
+const kittenTwoName = 'Fiona';
+const kittenTwoDesc =
+  ' Produce fascinación y curiosidad. Exótico, raro, bello, extraño... hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
+const kittenTwoRace = 'Sphynx';
+
+
+const kittenThreeImage = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenThreeName = 'Cielo';
+const kittenThreeDesc =
+  ' Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
+const kittenThreeRace = 'Maine Coon';
+
 
 const kittenOne = `<li class="card">
             <article>
               <img
                 class="card_img"
-                src="https://dev.adalab.es/gato-siames.webp"
+                src="${kittenOneImage}"
                 alt="siames-cat"
               />
-              <h3 class="card_title">Anastacio</h3>
-              <h4 class="card_race">Siamés</h4>
+              <h3 class="card_title">${kittenOneName}</h3>
+              <h4 class="card_race">${kittenOneRace}</h4>
               <p class="card_description">
-                Porte elegante, su patrón de color tan característico y sus ojos
-                de un azul intenso, pero su historia se remonta a Asía al menos
-                hace 500 años, donde tuvo su origen muy posiblemente.
+               ${kittenOneDesc}
               </p>
             </article>
           </li>`;
@@ -26,33 +49,41 @@ const kittenOne = `<li class="card">
 const kittenTwo = `<li class="card">
             <img
               class="card_img"
-              src="https://dev.adalab.es/sphynx-gato.webp"
+              src="${kittenTwoImage}"
               alt="sphynx-cat"
             />
-            <h3 class="card_title">Fiona</h3>
-            <h4 class="card_race">Sphynx</h4>
+            <h3 class="card_title">${kittenTwoName}</h3>
+            <h4 class="card_race">${kittenTwoRace}</h4>
             <p class="card_description">
-              Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-              hasta con pinta de alienígena han llegado a definir a esta raza
-              gatuna que se caracteriza por la «ausencia» de pelo.
+               ${kittenTwoDesc}
             </p>
           </li>`;
 
-const kittenThree = `li class="card">
+const kittenThree = `<li class="card">
             <img
               class="card_img"
-              src="https://dev.adalab.es/maine-coon-cat.webp"
+              src="${kittenThreeImage}"
               alt="maine-coon-cat"
             />
-            <h3 class="card_title">Cielo</h3>
-            <h4 class="card_race">Maine Coon</h4>
+            <h3 class="card_title">${kittenThreeName}</h3>
+            <h4 class="card_race">${kittenThreeRace}</h4>
             <p class="card_description">
-              Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-              bella mirada se ha convertido en una de sus señas de identidad.
-              Sus ojos son grandes y las orejas resultan largas y en punta.
+               ${kittenThreeDesc}
             </p>
           </li>`;
 
 /* para mostart el html siembre va al final*/
-
+/*
 list.innerHTML = kittenOne + kittenTwo + kittenThree;
+*/
+if( kittenOneDesc.includes(descrSearchText) ) {
+   list.innerHTML= kittenOne
+}
+
+if( kittenTwoDesc.includes(descrSearchText) ) {
+   list.innerHTML= kittenTwo
+}
+
+if( kittenThreeDesc.includes(descrSearchText) ) {
+list.innerHTML= kittenThree
+}
